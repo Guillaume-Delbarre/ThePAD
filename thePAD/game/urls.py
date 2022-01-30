6020a10774302f4f2bb8a4162a02e1e018e613d0
Guillaume Delbarre <guillaume.voolkan@gmail.com>
2022-01-30 10:33:28 +0100

    First Basic View

--- thePAD/game/urls.py
+++ thePAD/game/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:user_id>/', views.detail, name='detail'),
+    path('<int:user_id>/points/', views.points, name='points'),
+    path('<int:user_id>/attribute', views.attribute, name = 'attribute')
 ]
--- thePAD/game/views.py
+++ thePAD/game/views.py
@@ -1,5 +1,22 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import User
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    user_list = User.objects.all()
+    template = loader.get_template('game/index.html')
+    context = {
+        'user_list': user_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, user_id):
+    return HttpResponse("You're looking at user %s." % user_id)
+
+def points(request, user_id):
+    return HttpResponse("You're looking at the points of user %s." % user_id)
+
+def attribute(request, user_id):
+    return HttpResponse("You're adding point for user %s." % user_id)
