07f5f4581177e638bf1fd0c03c2819250cd6f2fa
Guillaume Delbarre <guillaume.voolkan@gmail.com>
2022-01-30 09:30:27 +0100

    First Model Creation

--- thePAD/game/models.py
+++ thePAD/game/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model) :
+    nom = models.CharField(max_length=50)
+    
+class Action(models.Model) :
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    points = models.IntegerField()
+    act_date = models.TimeField('action date')
