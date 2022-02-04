f640a0e7df308c666ba9da4ddc788f0cdd10e825
Guillaume Delbarre <guillaume.voolkan@gmail.com>
2022-02-04 20:00:23 +0100

    Admin creator modifications

--- thePAD/game/admin.py
+++ thePAD/game/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 from .models import User, Action
 
-admin.site.register(User)
-admin.site.register(Action)
+class ActionInline(admin.StackedInline) :
+    model = Action
+    extra = 1
+
+class UserAdmin(admin.ModelAdmin) :
+    fields = ['name']
+    inlines = [ActionInline]
+
+class ActionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields' : ['user']}),
+        ('Information date', {'fields' : ['act_date']}),
+        ('Valeurs',          {'fields' : ['point']})
+    ]
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Action, ActionAdmin)
