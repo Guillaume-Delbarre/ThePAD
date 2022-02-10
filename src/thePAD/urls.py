9a1dc7cc5ee0c0f45e74753dbb0a51b350e2b44e
Guillaume Delbarre <guillaume.voolkan@gmail.com>
2022-02-10 22:15:06 +0100

    Ajout de l'app authenticate et du login de user

--- src/thePAD/urls.py
+++ src/thePAD/urls.py
@@ -21,4 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('game.urls')),
     path('admin/', admin.site.urls),
+    path('participants/', include('django.contrib.auth.urls')),
+    path('participants/', include('participants.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
