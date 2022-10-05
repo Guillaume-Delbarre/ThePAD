bffcd56dddb0505d2b145edaa620e44560acf2e5
Guillaume Delbarre <guillaume.voolkan@gmail.com>
2022-10-05 21:37:02 +0200

    Premier Jet Global Chart

--- src/game/views.py
+++ src/game/views.py
@@ -8,9 +8,58 @@ from django.contrib.auth.models import User
 from .forms import PlayerForm, ActionForm
 from .models import Action, Player
 
+import json
+from datetime import date, datetime
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
+
 def index(request) :
     player_list = Player.objects.annotate(total_points=Sum('action__point')).order_by('-total_points')
-    return render(request, 'game/index.html', {'player_list' : player_list})
+    json_player_list = get_proper_JSON_from_player_list(player_list)
+    print(json_player_list)
+    return render(request, 'game/index.html', {'player_list' : player_list, 'json_player_list' : json_player_list})
+
+def get_proper_JSON_from_player_list(player_list) :
+    # Format du tableaux des actions :
+    #   Date | Score J1 | Score J2 | ...
+    tableau_final = []
+    names = []
+
+    # Première étape enregistrer toutes les actions
+    for player in player_list :
+        json_player = json.loads(player.getJSON())
+        names.append(json_player['name'])
+        for action in json_player["actions"] :
+            base_ligne = [datetime.fromisoformat(action['date_action'])] + [None]*len(player_list)
+            base_ligne[len(names)] = action['tot_score']
+            tableau_final.append(base_ligne)
+
+    # Process pour remplir le tableau
+    # Sort
+    tableau_final.sort(key= lambda row : row[0])
+    tableau_score = [0]*len(names)
+
+    player_list_dict = {'names' : names,
+                        'actions' : []}
+
+    for action in tableau_final :
+        for index, val in enumerate(action[1:]) :
+            if val == None :
+                action[index + 1] = tableau_score[index]
+            elif isinstance(val, int) :
+                tableau_score[index] = val
+        di = dict(zip(names, action[1:]))
+        di["Date"] = action[0]
+        player_list_dict["actions"].append(di)
+        
+    print(player_list_dict)
+
+    return json.dumps(player_list_dict, default=json_serial)
 
 def detail(request, player_id) :
     player = get_object_or_404(Player, pk=player_id)
